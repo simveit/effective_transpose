83a11e3dd179b7bdbb68610a261ffbec63165525
Simon Veitner <simp.veitner@gmail.com>
2025-05-02 18:05:22 +0200

    include code for profile

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 NVCC_FLAGS = -std=c++17 -O3 -DNDEBUG -w
 NVCC_LDFLAGS = -lcublas -lcuda
 OUT_DIR = out
+PROFILE_DIR = profile
 
 CUDA_OUTPUT_FILE = -o $(OUT_DIR)/$@
 NCU_PATH := $(shell which ncu)
-NCU_COMMAND = sudo $(NCU_PATH) --set full --import-source yes
+NCU_COMMAND = $(NCU_PATH) --set full --import-source yes
 
 NVCC_FLAGS += --expt-relaxed-constexpr --expt-extended-lambda --use_fast_math -Xcompiler=-fPIE -Xcompiler=-Wno-psabi -Xcompiler=-fno-strict-aliasing
 NVCC_FLAGS += -arch=sm_90a
@@ -23,6 +24,11 @@ transpose_swizzle: transpose_swizzle.cu
 transpose_swizzle_batched: transpose_swizzle_batched.cu 
 	$(NVCC_BASE) $^ $(CUDA_OUTPUT_FILE)
 
+transpose_swizzle_batched_for_profile: transpose_swizzle_batched_for_profile.cu 
+	$(NVCC_BASE) $^ $(CUDA_OUTPUT_FILE)
+
+transpose_swizzle_batched_profile: transpose_swizzle_batched_for_profile
+	$(NCU_COMMAND) -o $(PROFILE_DIR)/$@ -f $(OUT_DIR)/$^
 
 compile_all: 
 	make swizzle
---
+++ transpose_swizzle_batched_for_profile.cu
@@ -0,0 +1,237 @@
+#include <cuda.h>  // CUtensormap
+#include <cuda_runtime.h>
+
+#include <cmath>
+#include <cuda/barrier>
+#include <random>
+
+using barrier = cuda::barrier<cuda::thread_scope_block>;
+namespace cde = cuda::device::experimental;
+
+#include "utils.h"
+
+#define CHECK_CUDA_ERROR(val) check((val), #val, __FILE__, __LINE__)
+void check(cudaError_t err, char const *func, char const *file, int line) {
+  if (err != cudaSuccess) {
+    std::cerr << "CUDA Runtime Error at: " << file << ":" << line << std::endl;
+    std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+#define CHECK_LAST_CUDA_ERROR() checkLast(__FILE__, __LINE__)
+void checkLast(char const *file, int line) {
+  cudaError_t const err{cudaGetLastError()};
+  if (err != cudaSuccess) {
+    std::cerr << "CUDA Runtime Error at: " << file << ":" << line << std::endl;
+    std::cerr << cudaGetErrorString(err) << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+template <int BLOCK_SIZE>
+__device__ int calculate_col_swizzle(int row, int col) {
+  int i16 = (row * BLOCK_SIZE + col) * 4 >> 4;
+  int y16 = i16 >> 3;
+  int x16 = i16 & 7;
+  int x16_swz = y16 ^ x16;
+  return ((x16_swz * 4) & (BLOCK_SIZE - 1)) + (col & 3);
+}
+
+template <int BLOCK_SIZE>
+__device__ int calculate_row_swizzle(int row, int col) {
+  int i16_tr = (col * BLOCK_SIZE + row) * 4 >> 4;
+  int y16_tr = i16_tr >> 3;
+  int x16_tr = i16_tr & 7;
+  int x16_swz_tr = y16_tr ^ x16_tr;
+  return ((x16_swz_tr * 4) & (BLOCK_SIZE - 1)) + (row & 3);
+}
+
+template <int BLOCK_SIZE, int LOG_BLOCK, int BATCH_SIZE, int LOG_BATCH_SIZE>
+__global__ void kernel(const __grid_constant__ CUtensorMap tensor_map,
+                       const __grid_constant__ CUtensorMap tensor_map_tr) {
+  // The destination shared memory buffer of a bulk tensor operation should be
+  // 128 byte aligned.
+  __shared__ alignas(1024) float smem_buffer[BLOCK_SIZE * BLOCK_SIZE];
+  __shared__ alignas(1024) float smem_buffer_tr[BLOCK_SIZE * BLOCK_SIZE];
+  // Coordinates for upper left tile in GMEM.
+  int x = blockIdx.x * BLOCK_SIZE;
+  int y = blockIdx.y * BLOCK_SIZE;
+
+  int col = (threadIdx.x & (BLOCK_SIZE / BATCH_SIZE - 1)) * BATCH_SIZE;
+  int row = threadIdx.x >> (LOG_BLOCK - LOG_BATCH_SIZE);
+
+// Initialize shared memory barrier with the number of threads participating in
+// the barrier.
+#pragma nv_diag_suppress static_var_with_dynamic_init
+  __shared__ barrier bar;
+
+  if (threadIdx.x == 0) {
+    // Initialize barrier. All `blockDim.x` threads in block participate.
+    init(&bar, blockDim.x);
+    // Make initialized barrier visible in async proxy.
+    cde::fence_proxy_async_shared_cta();
+  }
+  // Syncthreads so initialized barrier is visible to all threads.
+  __syncthreads();
+
+  barrier::arrival_token token;
+  if (threadIdx.x == 0) {
+    // Initiate bulk tensor copy.
+    cde::cp_async_bulk_tensor_2d_global_to_shared(&smem_buffer, &tensor_map, x,
+                                                  y, bar);
+    // Arrive on the barrier and tell how many bytes are expected to come in.
+    token = cuda::device::barrier_arrive_tx(bar, 1, sizeof(smem_buffer));
+  } else {
+    // Other threads just arrive.
+    token = bar.arrive();
+  }
+  // Wait for the data to have arrived.
+  bar.wait(std::move(token));
+
+// Transpose tile.
+#pragma unroll
+  for (int j = 0; j < BATCH_SIZE; j++) {
+    int col_ = col + j;
+    int row_ = row;
+    int col_swizzle = calculate_col_swizzle<BLOCK_SIZE>(row_, col_);
+    int row_swizzle = calculate_row_swizzle<BLOCK_SIZE>(row_, col_);
+
+    smem_buffer_tr[col_ * BLOCK_SIZE + row_swizzle] =
+        smem_buffer[row_ * BLOCK_SIZE + col_swizzle];
+  }
+  // Wait for shared memory writes to be visible to TMA engine.
+  cde::fence_proxy_async_shared_cta();
+  __syncthreads();
+  // After syncthreads, writes by all threads are visible to TMA engine.
+
+  // Initiate TMA transfer to copy shared memory to global memory
+  if (threadIdx.x == 0) {
+    // Transpose tile inside matrix
+    cde::cp_async_bulk_tensor_2d_shared_to_global(&tensor_map_tr, y, x,
+                                                  &smem_buffer_tr);
+    // Wait for TMA transfer to have finished reading shared memory.
+    // Create a "bulk async-group" out of the previous bulk copy operation.
+    cde::cp_async_bulk_commit_group();
+    // Wait for the group to have completed reading from shared memory.
+    cde::cp_async_bulk_wait_group_read<0>();
+  }
+
+  // Destroy barrier. This invalidates the memory region of the barrier. If
+  // further computations were to take place in the kernel, this allows the
+  // memory location of the shared memory barrier to be reused.
+  if (threadIdx.x == 0) {
+    (&bar)->~barrier();
+  }
+}
+
+int main() {
+  const int GMEM_WIDTH = 32768;
+  const int GMEM_HEIGHT = 32768;
+  const int BLOCK_SIZE = 32;
+  const int LOG_BLOCK = 5;
+  const int BATCH_SIZE = 8;
+  const int LOG_BATCH_SIZE = 3;
+  const int SMEM_WIDTH = BLOCK_SIZE;
+  const int SMEM_HEIGHT = BLOCK_SIZE;
+  const size_t SIZE = GMEM_HEIGHT * GMEM_WIDTH * sizeof(float);
+
+  float *h_in = new float[GMEM_HEIGHT * GMEM_WIDTH];
+  float *h_out = new float[GMEM_HEIGHT * GMEM_WIDTH];
+
+  // Initialize with normal distribution
+  std::default_random_engine generator(42);
+  std::normal_distribution<float> distribution(0.0, 1.0);
+
+  for (int i = 0; i < GMEM_HEIGHT * GMEM_WIDTH; ++i) {
+    h_in[i] = distribution(generator);
+  }
+
+  float *d;
+  float *d_tr;
+  CHECK_CUDA_ERROR(cudaMalloc(&d, SIZE));
+  CHECK_CUDA_ERROR(cudaMemcpy(d, h_in, SIZE, cudaMemcpyHostToDevice));
+  void *tensor_ptr = (void *)d;
+  CHECK_CUDA_ERROR(cudaMalloc(&d_tr, SIZE));
+  void *tensor_ptr_tr = (void *)d_tr;
+
+  CUtensorMap tensor_map{};
+  CUtensorMap tensor_map_tr{};
+  // rank is the number of dimensions of the array.
+  constexpr uint32_t rank = 2;
+  uint64_t size[rank] = {GMEM_WIDTH, GMEM_HEIGHT};
+  uint64_t size_tr[rank] = {GMEM_HEIGHT, GMEM_WIDTH};
+  // The stride is the number of bytes to traverse from the first element of one
+  // row to the next. It must be a multiple of 16.
+  uint64_t stride[rank - 1] = {GMEM_WIDTH * sizeof(float)};
+  uint64_t stride_tr[rank - 1] = {GMEM_HEIGHT * sizeof(float)};
+  // The box_size is the size of the shared memory buffer that is used as the
+  // destination of a TMA transfer.
+  uint32_t box_size[rank] = {SMEM_WIDTH, SMEM_HEIGHT};
+  uint32_t box_size_tr[rank] = {SMEM_HEIGHT, SMEM_WIDTH};
+  // The distance between elements in units of sizeof(element). A stride of 2
+  // can be used to load only the real component of a complex-valued tensor, for
+  // instance.
+  uint32_t elem_stride[rank] = {1, 1};
+
+  // Create the tensor descriptor.
+  CUresult res = cuTensorMapEncodeTiled(
+      &tensor_map,  // CUtensorMap *tensorMap,
+      CUtensorMapDataType::CU_TENSOR_MAP_DATA_TYPE_FLOAT32,
+      rank,         // cuuint32_t tensorRank,
+      tensor_ptr,   // void *globalAddress,
+      size,         // const cuuint64_t *globalDim,
+      stride,       // const cuuint64_t *globalStrides,
+      box_size,     // const cuuint32_t *boxDim,
+      elem_stride,  // const cuuint32_t *elementStrides,
+      CUtensorMapInterleave::CU_TENSOR_MAP_INTERLEAVE_NONE,
+      CUtensorMapSwizzle::CU_TENSOR_MAP_SWIZZLE_128B,
+      CUtensorMapL2promotion::CU_TENSOR_MAP_L2_PROMOTION_NONE,
+      CUtensorMapFloatOOBfill::CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE);
+
+  assert(res == CUDA_SUCCESS);
+
+  CUresult res_tr = cuTensorMapEncodeTiled(
+      &tensor_map_tr,  // CUtensorMap *tensorMap,
+      CUtensorMapDataType::CU_TENSOR_MAP_DATA_TYPE_FLOAT32,
+      rank,           // cuuint32_t tensorRank,
+      tensor_ptr_tr,  // void *globalAddress,
+      size_tr,        // const cuuint64_t *globalDim,
+      stride,         // const cuuint64_t *globalStrides,
+      box_size_tr,    // const cuuint32_t *boxDim,
+      elem_stride,    // const cuuint32_t *elementStrides,
+      CUtensorMapInterleave::CU_TENSOR_MAP_INTERLEAVE_NONE,
+      CUtensorMapSwizzle::CU_TENSOR_MAP_SWIZZLE_128B,
+      CUtensorMapL2promotion::CU_TENSOR_MAP_L2_PROMOTION_NONE,
+      CUtensorMapFloatOOBfill::CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE);
+
+  assert(res_tr == CUDA_SUCCESS);
+
+  dim3 blockDim((SMEM_WIDTH * SMEM_HEIGHT) / BATCH_SIZE, 1, 1);
+  dim3 gridDim(GMEM_WIDTH / SMEM_WIDTH, GMEM_HEIGHT / SMEM_HEIGHT, 1);
+
+  kernel<BLOCK_SIZE, LOG_BLOCK, BATCH_SIZE, LOG_BATCH_SIZE>
+      <<<gridDim, blockDim>>>(tensor_map, tensor_map_tr);
+
+  CHECK_LAST_CUDA_ERROR();
+  CHECK_CUDA_ERROR(cudaMemcpy(h_out, d_tr, SIZE, cudaMemcpyDeviceToHost));
+
+  const float epsilon = 1e-5f;
+  for (int x = 0; x < GMEM_HEIGHT; x++) {
+    for (int y = 0; y < GMEM_WIDTH; y++) {
+      float expected = h_in[x * GMEM_WIDTH + y];
+      float actual = h_out[y * GMEM_HEIGHT + x];
+      if (std::fabs(expected - actual) > epsilon) {
+        std::cout << "Error at position (" << x << "," << y << "): expected "
+                  << expected << " but got " << actual << std::endl;
+        return -1;
+      }
+    }
+  }
+
+  std::cout << "Passed" << std::endl;
+
+  CHECK_CUDA_ERROR(cudaFree(d));
+  free(h_in);
+  free(h_out);
+}
